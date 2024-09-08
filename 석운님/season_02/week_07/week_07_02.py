f278fd3df9f236617bb570cd5670057b980fed07
jseokun <jswboseok@gmail.com>
2024-09-08 10:24:02 +0900

    new week 07 02

--- 석운님/season_02/week_07/week_07_02.py
+++ 석운님/season_02/week_07/week_07_02.py
@@ -5,11 +5,9 @@ from collections import deque
 # 편의를 위해 부모노드는 무조건 자식노드보다 낮은 번호를 가진다.
 def make_tree(n):  # 트리의 크기
     P = [-1] + [random.randint(0, i - 1) for i in range(1, n)]
-
     return P
 
 path = []
-
 
 def make_binary_tree(n):  # 이진트리 생성 함수
     P = [-1]
@@ -22,13 +20,11 @@ def make_binary_tree(n):  # 이진트리 생성 함수
         S.append(0)
     return P
 
-
 def make_complete_binary_tree(n):
     # return [(i+1)//2-1 for i in range(n)]
     # 이해를 돕기 위해 아래 코드를 사용한다.
     # 더미로 생기는 0번 노드는 무시한다.
     return [i // 2 for i in range(n + 1)]
-
 
 def make_picture(tree):  # 시각화 함수
     N = len(tree)
@@ -64,22 +60,60 @@ cvs.pack()
 def find_path(s, e, tree):
 
     q = deque()
-    q.append((s, [s]))
+    q.append((s, 0))
+    #q.append((s, [s]))
     visited = [False for _ in range(len(tree))]
     visited[s] = True
     while q:
-        now, path = q.popleft()
+        now, cnt = q.popleft()
+        #now, path = q.popleft()
         #print(now, path)
         if now == e:
-            return path
+            return cnt
+            #return path
         for ne in range(now + 1, len(tree)):
             if tree[ne] == now and not visited[ne]:
-                q.append((ne, path + [ne]))
+                q.append((ne, cnt + 1))
                 visited[ne] = True
         if not visited[tree[now]]:
-            q.append((tree[now], path + [tree[now]]))
+            q.append((tree[now], cnt + 1))
             visited[tree[now]] = True
 
+
+def find_max_path(tree):
+    node = 0
+    q = deque()
+    q.append((0, 0))
+    # find the deepest leaf node
+    maxcnt, maxnode = 0, 0
+    while q:
+        node, cnt = q.popleft()
+        if cnt > maxcnt:
+            maxcnt = cnt
+            maxnode = node
+        for ne in range(node + 1, len(tree)):
+            if tree[ne] == node:
+                q.append((ne, cnt + 1))
+    start = node
+
+    # find the deepest leaf node
+    q = deque()
+    q.append((start, 0))
+    visited = [False for _ in range(len(tree))]
+    visited[start] = True
+    ans = -1
+    while q:
+        now, cnt = q.popleft()
+        ans = max(ans, cnt)
+        #print(now, path)
+        for ne in range(now + 1, len(tree)):
+            if tree[ne] == now and not visited[ne]:
+                q.append((ne, cnt + 1))
+                visited[ne] = True
+        if not visited[tree[now]]:
+            q.append((tree[now], cnt + 1))
+            visited[tree[now]] = True
+    return ans
 
 tree1 = make_tree(15)
 tree2 = make_binary_tree(15)
@@ -87,8 +121,12 @@ tree3 = make_complete_binary_tree(15)
 print(tree3)
 
 p = find_path(2, 7, tree1)
+mp = find_max_path(tree1)
 print(tree1)
 print(p)
+print(mp)
 p = find_path(2, 7, tree2)
+mp = find_max_path(tree1)
 print(tree2)
 print(p)
+print(mp)
